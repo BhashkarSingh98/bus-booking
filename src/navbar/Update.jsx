a5ad8cdb6cddf2556cefc7fe9520039c88da2062
bhashkar <bhashkar2600@gmail.com>
2022-12-27 23:29:20 +0530

    dfsdwdqwd

--- src/navbar/Update.jsx
+++ src/navbar/Update.jsx
@@ -11,12 +11,13 @@ const Update = () => {
   const navigate = useNavigate();
 
   useEffect(() => {
-    
     getproductdet();
-  },[ ]);
+  }, [getproductdet,updateproduct]);
 
   const getproductdet = async () => {
-    let result = await fetch(`https://long-tan-crane-hem.cyclic.app/passenger/${params.id}`);
+    let result = await fetch(
+      `https://long-tan-crane-hem.cyclic.app/passenger/${params.id}`
+    );
     result = await result.json();
 
     console.log(result);
@@ -27,67 +28,59 @@ const Update = () => {
 
   const updateproduct = async () => {
     console.log(name, gender, seatNo);
-    let result = await fetch(`https://long-tan-crane-hem.cyclic.app/passenger/${params.id}`, {
-      method: "Put",
-      body: JSON.stringify({ name, gender, seatNo }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
+    let result = await fetch(
+      `https://long-tan-crane-hem.cyclic.app/passenger/${params.id}`,
+      {
+        method: "Put",
+        body: JSON.stringify({ name, gender, seatNo }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
     result = await result.json();
     console.log(result);
-    alert("Ticket Confirmed ")
+    alert("Ticket Confirmed ");
     navigate("/booknow");
   };
 
   return (
     <div>
       <div className="update">
-        <div className="from-box-1">
-
-        </div>
+        <div className="from-box-1"></div>
         <div className="login-box-2">
-      <h1 className="register">Details</h1>
-      
-      <input
-        type="text"
-        className="inputbox"
-        placeholder="NAME"
-        value={name}
-        onChange={(e) => setname(e.target.value)}
-      />
+          <h1 className="register">Details</h1>
 
-      <input
-        type="text"
-        className="inputbox"
-        placeholder="GENDER"
-        value={gender}
-        onChange={(e) => setgender(e.target.value)}
-      />
+          <input
+            type="text"
+            className="inputbox"
+            placeholder="NAME"
+            value={name}
+            onChange={(e) => setname(e.target.value)}
+          />
 
-      <input
-        type="text"
-        className="inputbox"
-        placeholder="SEAT-NO"
-        value={seatNo}
-        
-      />
+          <input
+            type="text"
+            className="inputbox"
+            placeholder="GENDER"
+            value={gender}
+            onChange={(e) => setgender(e.target.value)}
+          />
 
+          <input
+            type="text"
+            className="inputbox"
+            placeholder="SEAT-NO"
+            value={seatNo}
+          />
 
-      <button className="buttonsignup" onClick={updateproduct}>
-        CONFIRM
-      </button>
-    </div>
-    </div>
+          <button className="buttonsignup" onClick={updateproduct}>
+            CONFIRM
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
 
 export default Update;
-
-
-
-
-
-
-
