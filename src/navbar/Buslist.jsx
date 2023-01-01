a408912533748fa5b0f920b21406d32e176ad401
bhashkar <bhashkar2600@gmail.com>
2023-01-01 16:01:27 +0530

    sadas

--- src/navbar/Buslist.jsx
+++ src/navbar/Buslist.jsx
@@ -3,6 +3,9 @@ import { Link } from "react-router-dom";
 import "../css/Buslist.css";
 
 const Buslist = () => {
+    let date=new Date()
+    let currdate=date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate();
+    console.log(currdate);
   return (
     <div>
         <div className="the-lesest" style={{marginTop:"20px",marginBottom:"30px"}}>
@@ -16,14 +19,14 @@ const Buslist = () => {
             src="https://ak.picdn.net/shutterstock/videos/1068643934/thumb/6.jpg?ip=x480"
             alt=""
           />
-          <p className="busname">Volvo</p>
+          <p className="busname">Delux</p>
           <p className="buslist-add1">
             <span>Dehradun</span> <span>-To- </span>
-            <span>Nainital</span>
+            <span>Bageshwar</span>
           </p>
           <p className="buslist-bustime">
-            <span>Time - </span>
-            <span>01/01/2023</span>
+            <span>Date - </span>
+            <span>{currdate}</span>
           </p>
           <Link to="/bookseat">
             <button className="buslist-btn">Book Now</button>
@@ -35,12 +38,14 @@ const Buslist = () => {
             src="https://media.istockphoto.com/id/1371319562/photo/blue-bus-moving-on-the-road-in-city-in-early-morning.jpg?b=1&s=170667a&w=0&k=20&c=A0AGQw5p7rPyLsLT42VPg3hWqyAf6OnE1FdrjtTN3wU="
             alt=""
           />
-          <p className="busname">Volvo</p>
-          <span>Dehradun</span> <span>-To- </span>
-          <span>Nainital</span>
+          <p className="busname">Indigo</p>
+          <p className="buslist-add1">
+            <span>Dehradun</span> <span>-To- </span>
+            <span>Mnali</span>
+          </p>
           <p className="buslist-bustime">
-            <span>Time - </span>
-            <span>01/01/2023</span>
+            <span>Date - </span>
+            <span>{currdate}</span>
           </p>
           <Link to="/bookseat">
             <button className="buslist-btn">Book Now</button>
@@ -52,12 +57,14 @@ const Buslist = () => {
             src="https://media.istockphoto.com/id/540124958/photo/coach-bus.jpg?b=1&s=170667a&w=0&k=20&c=WJQ7FMF_kW_bldWe7b0_WLvV45ZiIbezhQEO2B4xsSI="
             alt=""
           />
-          <p className="busname">Volvo</p>
-          <span>Dehradun</span> <span>-To- </span>
-          <span>Nainital</span>
+          <p className="busname">Intra City</p>
+          <p className="buslist-add1">
+            <span>Dehradun</span> <span>-To- </span>
+            <span>Shimla</span>
+          </p>
           <p className="buslist-bustime">
-            <span>Time - </span>
-            <span>01/01/2023</span>
+            <span>Date - </span>
+            <span>{currdate}</span>
           </p>
           <Link to="/bookseat">
             <button className="buslist-btn">Book Now</button>
@@ -69,12 +76,14 @@ const Buslist = () => {
             src="https://media.istockphoto.com/id/1372188593/photo/modern-city-bus.jpg?b=1&s=170667a&w=0&k=20&c=-2D1HiCF5NCus9CWUnziiTeKD_x8n1tmiZfJJ_GDzXk="
             alt=""
           />
-          <p className="busname">Volvo</p>
-          <span>Dehradun</span> <span>-To- </span>
-          <span>Nainital</span>
+          <p className="busname">Himalayn</p>
+          <p className="buslist-add1">
+            <span>Dehradun</span> <span>-To- </span>
+            <span>Goa</span>
+          </p>
           <p className="buslist-bustime">
-            <span>Time - </span>
-            <span>01/01/2023</span>
+            <span>Date - </span>
+            <span>{currdate}</span>
           </p>
           <Link to="/bookseat">
             <button className="buslist-btn">Book Now</button>
@@ -87,11 +96,13 @@ const Buslist = () => {
             alt=""
           />
           <p className="busname">Volvo</p>
-          <span>Dehradun</span> <span>-To- </span>
-          <span>Nainital</span>
+          <p className="buslist-add1">
+            <span>Dehradun</span> <span>-To- </span>
+            <span>Nainital</span>
+          </p>
           <p className="buslist-bustime">
-            <span>Time - </span>
-            <span>01/01/2023</span>
+            <span>Date - </span>
+            <span>{currdate}</span>
           </p>
           <Link to="/bookseat">
             <button className="buslist-btn">Book Now</button>
@@ -103,12 +114,14 @@ const Buslist = () => {
             src="https://images.unsplash.com/photo-1600198741448-fc40d918673a?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NzZ8fGJ1c3xlbnwwfHwwfHw%3D&auto=format&fit=crop&w=600&q=60"
             alt=""
           />
-          <p className="busname">Volvo</p>
-          <span>Dehradun</span> <span>-To- </span>
-          <span>Nainital</span>
+          <p className="busname">Vgo Bus</p>
+          <p className="buslist-add1">
+            <span>Dehradun</span> <span>-To- </span>
+            <span>Delhi</span>
+          </p>
           <p className="buslist-bustime">
-            <span>Time - </span>
-            <span>01/01/2023</span>
+            <span>Date - </span>
+            <span>{currdate}</span>
           </p>
           <Link to="/bookseat">
             <button className="buslist-btn">Book Now</button>
